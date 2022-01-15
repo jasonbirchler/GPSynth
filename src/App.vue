77de5262b68e0e02ee10ffeef81546c3897c41ec
Jason Birchler <jason@Jasons-MacBook-Pro-2.local>
2022-01-14 17:42:00 -0800

    basic Vue setup

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <GPSynth msg="Synth goes here"/>
+  </div>
+</template>
+
+<script>
+import GPSynth from './components/GPSynth.vue'
+
+export default {
+  name: 'App',
+  components: {
+    GPSynth
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
